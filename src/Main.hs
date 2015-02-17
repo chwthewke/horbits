e21738ed5ad604f3f62fcf6724b9ebb30b508944
Chewie <chwthewke@gmail.com>
2015-02-17 18:01:33 +0100

    [horbits] We have an OpenGL triangle

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
-import           Control.Lens           hiding (set)
+import           Control.Lens                 hiding (set)
+import           Graphics.Rendering.OpenGL.GL
 import           Graphics.UI.Gtk
+import           Graphics.UI.Gtk.OpenGL
 import           Horbits.Body
 import           Horbits.SolarSystem
 import           Horbits.UI.BodyDetails
@@ -14,6 +16,18 @@ main = do
     _ <- set window [ windowTitle := "Hello" ]
     box <- hBoxNew True 5
     containerAdd window box
+    -- Actual layout
+    containerAdd box =<< bodyDataPane
+    containerAdd box =<< initGlCanvas 600 400 drawCanvas
+    --
+    widgetShowAll window
+    _ <- onDestroy window mainQuit
+    mainGUI
+
+
+bodyDataPane :: IO VBox
+bodyDataPane = do
+    box <- vBoxNew True 5
     bodyList <- bodyListNew bodiesTree
     containerAdd box $ bodyListView bodyList
     bodyDetailsPane <- bodyDetailsPaneNew
@@ -21,6 +35,43 @@ main = do
         putStrLn $ "Selected " ++ b ^. bodyName
         bodyDetailsPaneSetBody bodyDetailsPane b)
     containerAdd box $ bodyDetailsPaneView bodyDetailsPane
-    widgetShowAll window
-    _ <- onDestroy window mainQuit
-    mainGUI
+    return box
+
+
+
+initGlCanvas :: Int -> Int -> IO() -> IO GLDrawingArea
+initGlCanvas w h draw = do
+    glConfig <- glConfigNew [GLModeRGBA, GLModeDepth, GLModeDouble]
+    canvas <- glDrawingAreaNew glConfig
+    widgetSetSizeRequest canvas w h
+    _ <- onRealize canvas $ withGLDrawingArea canvas setupProj
+    setupDrawLoop canvas draw
+    return canvas
+
+setupProj :: GLWindow -> IO ()
+setupProj _ = do
+  clearColor $= Color4 0.0 0.0 1.0 0.0
+  matrixMode $= Projection
+  loadIdentity
+  ortho 0.0 1.0 0.0 1.0 (-1.0) 1.0
+  depthFunc $= Just Less
+  drawBuffer $= BackBuffers
+
+setupDrawLoop :: GLDrawingArea -> IO () -> IO ()
+setupDrawLoop canvas draw = do
+    _ <- onExpose canvas . const $ withGLDrawingArea canvas drawLoop >> return True
+    _ <- timeoutAddFull (widgetQueueDraw canvas >> return True) priorityDefaultIdle 8
+    return ()
+  where
+    drawLoop glWin = do
+        clear [DepthBuffer, ColorBuffer]
+        draw
+        glDrawableSwapBuffers glWin
+
+drawCanvas :: IO ()
+drawCanvas = do
+    color (Color3 1.0 1.0 1.0 :: Color3 GLfloat)
+    renderPrimitive Polygon $ do
+        vertex (Vertex3 0.0 0.0 0.0 :: Vertex3 GLfloat)
+        vertex (Vertex3 1.0 0.0 0.0 :: Vertex3 GLfloat)
+        vertex (Vertex3 0.0 1.0 0.0 :: Vertex3 GLfloat)
