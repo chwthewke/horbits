d5cd3f8c516a762698f5a62daa8d738bc2228be6
Chewie <chwthewke@gmail.com>
2015-02-26 17:20:38 +0100

    [horbits] Rewrite of body details pane
    with pretty-printing and a nicer model
    
    BD

--- src/Horbits/UI/BodyDetails.hs
+++ src/Horbits/UI/BodyDetails.hs
@@ -1,58 +1,87 @@
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances         #-}
 {-# LANGUAGE Rank2Types                #-}
-{-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE TypeSynonymInstances      #-}
 
 module Horbits.UI.BodyDetails where
 
-import           Control.Lens
+import           Control.Applicative
+import           Control.Lens                         hiding ((*~))
 import           Control.Monad                        hiding (ap, forM_, mapM_, sequence_)
 import           Data.Foldable
-import           Graphics.UI.Gtk as Gtk
+import           Graphics.UI.Gtk                      as Gtk
 import           Horbits.Body
 import           Horbits.Orbit
 import           Horbits.SolarSystem
+import           Horbits.UI.ShowDim
 import           Numeric.Units.Dimensional.TF.Prelude hiding (mapM_, sequence_)
-import           Prelude                              hiding (mapM_, sequence_, (+))
+import           Prelude                              hiding (mapM_, sequence_, (+), (/))
 
--- TODO layout and rendering (Show (Dimensional v d) not enough)
+-- TODO layout (sizes, L/R align)
+-- TODO VScroll
 
 -- Detail definitions
-data Detail s = forall a. (Show a) => Detail (Maybe String) a (Getting a s a)
-data DetailsSection = forall s. DetailsSection (Maybe String) (Fold Body s) [Detail s]
+
+data Detail s a = Detail (Maybe String) (Maybe a) (Fold s a)
+
+instance Functor (Detail s) where
+    fmap f (Detail t d p) = Detail t (f <$> d) (p . to f)
+
+detailOf :: Fold s a -> Detail s a
+detailOf = Detail Nothing Nothing
+
+titled :: String -> Detail s a -> Detail s a
+titled t (Detail _ d p) = Detail (Just t) d p
+
+defaulting :: a -> Detail s a -> Detail s a
+defaulting d (Detail t _ p) = Detail t (Just d) p
+
+
+data DetailsSection = forall s. DetailsSection (Maybe String) (Fold Body s) [Detail s String]
+
 
 -- Single Line
 data BodyDetailLabel a = BodyDetailLabel { bodyDetailLabelView    :: HBox
                                          , bodyDetailLabelSetData :: a -> IO ()
                                          }
 
-bodyDetailLabelNew :: Detail a -> IO (BodyDetailLabel a)
+bodyDetailLabelNew :: Detail s String -> IO (BodyDetailLabel s)
 bodyDetailLabelNew (Detail name def prop) = do
     hb <- hBoxNew True 5
+    -- title
     forM_ name (\n -> labelNew (Just n) >>= containerAdd hb)
-    valueLabel <- labelNew . Just $ show def
+    -- label
+    valueLabel <- labelNew Nothing
     _ <- containerAdd hb valueLabel
-    return $ BodyDetailLabel hb (labelSetText valueLabel . show . view prop)
+    -- label update
+    let update mv = do
+        forM_ mv $ labelSetMarkup valueLabel
+        Gtk.set hb [ widgetVisible := has _Just mv ]
+    _ <- update def
+    return $ BodyDetailLabel hb (update . preview prop)
 
 
 -- Section
-data BodyDetailsSectionWidgets = BodyDetailsSectionWidgets { bodyDetailsSectionWidgets :: [Widget]
-                                                           , bodyDetailsSectionSetBody :: Body -> IO () }
+data BodyDetailsSectionBox = BodyDetailsSectionBox { bodyDetailsSectionView :: VBox
+                                                   , bodyDetailsSectionSetBody :: Body -> IO () }
 
-titleLabel :: Maybe String -> IO [Widget]
-titleLabel ms = forM (ms ^.. _Just) (\s -> fmap toWidget $ do
+titleLabel :: String -> IO Label
+titleLabel t = do
     l <- labelNew Nothing
-    labelSetMarkup l $ "<b>" ++ s ++ "</b>"
-    return l)
+    labelSetMarkup l $ "<b>" ++ t ++ "</b>"
+    return l
 
-bodyDetailsSectionLabels :: DetailsSection -> IO BodyDetailsSectionWidgets
-bodyDetailsSectionLabels (DetailsSection title prop details) = do
-    titleWidget <- titleLabel title
+bodyDetailsSectionNew :: DetailsSection -> IO BodyDetailsSectionBox
+bodyDetailsSectionNew (DetailsSection title prop details) = do
+    vb <- vBoxNew True 5
+    forM_ title $ containerAdd vb <=< titleLabel
     detailLabels <- mapM bodyDetailLabelNew details
-    let widgets = titleWidget ++ fmap (toWidget . bodyDetailLabelView) detailLabels
-    let setBody b = let propValue = b ^? prop in do
-        forM_ propValue (forM_ detailLabels . flip bodyDetailLabelSetData)
-        forM_ widgets $ flip Gtk.set [ widgetVisible := has _Just propValue ]
-    return $ BodyDetailsSectionWidgets widgets setBody
+    forM_ detailLabels $ containerAdd vb . bodyDetailLabelView
+    return $ BodyDetailsSectionBox vb (setBody vb detailLabels)
+  where
+    setBody box labels b = do
+        forM_ (b ^? prop) (forM_ labels . flip bodyDetailLabelSetData)
+        Gtk.set box [ widgetVisible := has _Just (b ^? prop) ]
 
 -- Whole details pane
 data BodyDetailsPane = BodyDetailsPane { bodyDetailsPaneView    :: VBox
@@ -61,53 +90,54 @@ data BodyDetailsPane = BodyDetailsPane { bodyDetailsPaneView    :: VBox
 
 bodyDetailsSections :: [DetailsSection] -> IO BodyDetailsPane
 bodyDetailsSections sections = do
-    pane <- vBoxNew True 5
-    detailsWidgets <- forM sections bodyDetailsSectionLabels
-    forM_ (detailsWidgets >>= bodyDetailsSectionWidgets) (containerAdd pane)
-    let setBody = forM_ detailsWidgets . flip bodyDetailsSectionSetBody 
+    pane <- vBoxNew False 5
+    detailsWidgets <- forM sections bodyDetailsSectionNew
+    forM_ detailsWidgets (containerAdd pane . bodyDetailsSectionView)
+    let setBody = forM_ detailsWidgets . flip bodyDetailsSectionSetBody
     return $ BodyDetailsPane pane setBody
 
 bodyDetailsPaneNew :: IO BodyDetailsPane
 bodyDetailsPaneNew = bodyDetailsSections detailsSections
 
 -- Actual content
+--
+
 
 headerSection :: DetailsSection
 headerSection = DetailsSection Nothing (to id)
-    [ Detail Nothing "" bodyName
-    , Detail Nothing "" (bodyId . to (show . bodyPosition))
+    [ detailOf bodyName
+    , detailOf bodyId <&> bodyPosition <&> showBodyPosition
     ]
-
 
 orbitalSection :: DetailsSection
 orbitalSection = DetailsSection (Just "Orbital Characteristics") (bodyId . bodyOrbit)
-    [ Detail (Just "Semi-major axis") _0 semiMajorAxis
-    , Detail (Just "Apoapsis") _0 apoapsisHeight
-    , Detail (Just "Periapsis") _0 periapsisHeight
-    , Detail (Just "Orbital Eccentricity") _0 eccentricity
-    , Detail (Just "Orbital Inclination") _0 inclination
-    , Detail (Just "Argument of periapsis") _0 argumentOfPeriapsis
-    , Detail (Just "Longitude of asc. node") _0 rightAscensionOfAscendingNode
-    , Detail (Just "Mean anomaly at epoch") _0 meanAnomalyAtEpoch
-    , Detail (Just "Sidereal orbital period") _0 orbitalPeriod
-    , Detail (Just "Orbital velocity") (OrbitalVelocity _0 _0) orbitalVelocity
+    [ detailOf semiMajorAxis & titled "Semi-major axis" <&> showOrbitalDistance
+    , detailOf apoapsisHeight & titled "Apoapsis" <&> showOrbitalDistance
+    , detailOf periapsisHeight & titled "Periapsis" <&> showOrbitalDistance
+    , detailOf eccentricity & titled "Eccentricity" <&> showDegreeAngle
+    , detailOf inclination & titled "Inclination" <&> showDegreeAngle
+    , detailOf argumentOfPeriapsis & titled "Argument of periapsis" <&> showDegreeAngle
+    , detailOf rightAscensionOfAscendingNode & titled "Longitude of asc. node" <&> showDegreeAngle
+    , detailOf meanAnomalyAtEpoch & titled "Longitude of asc. node" <&> showRadianAngle
+    , detailOf orbitalPeriod & titled "Orbital period" <&> showKerbalTime
+    , detailOf orbitalVelocity & titled "Orbital Velocity" <&> showOrbitalVelocity
     ]
 
 physicalSection :: DetailsSection
 physicalSection = DetailsSection (Just "Physical Characteristics") (to id)
-    [ Detail (Just "Equatorial radius") _0 bodyRadius
-    , Detail (Just "Surface area") _0 bodySurfaceArea
-    , Detail (Just "Mass") _0 bodyMass
-    , Detail (Just "Gravitational parameter") _0 bodyGravitationalParam
-    , Detail (Just "Density") _0 bodyDensity
-    , Detail (Just "Surface gravity") _0 bodySurfaceGravity
-    , Detail (Just "Escape velocity") _0 bodyEscapeVelocity
-    , Detail (Just "Sidereal rotation period") _0 bodySiderealRotationPeriod
-    , Detail (Just "Sidereal rotation velocity") _0 bodySiderealRotationVelocity
-    , Detail (Just "Solar day") Nothing . pre $ bodySolarDay
-    , Detail (Just "Synchronous orbit") _0 bodySynchronousOrbitAltitude
-    , Detail (Just "Sphere of influence") Nothing . pre $ bodySphereOfInfluence
-    ] 
+    [ detailOf bodyRadius & titled "Equatorial radius" <&> showPlanetaryDistance
+    , detailOf bodySurfaceArea & titled "Surface area" <&> showQuantitySci
+    , detailOf bodyMass & titled "Mass" <&> showQuantitySci
+    , detailOf bodyGravitationalParam & titled "Gravitational parameter" <&> showQuantitySci
+    , detailOf bodyDensity & titled "Density" <&> showQuantity
+    , detailOf bodySurfaceGravity & titled "Surface gravity" <&> showQuantity
+    , detailOf bodyEscapeVelocity & titled "Escape velocity" <&> showQuantity
+    , detailOf bodySiderealRotationPeriod & titled "Sidereal rotation period" <&> showKerbalTime
+    , detailOf bodySiderealRotationVelocity & titled "Sidereal rotation velocity" <&> showQuantity
+    , detailOf bodySolarDay & titled "Solar Day" <&> showKerbalTime
+    , detailOf bodySynchronousOrbitAltitude & titled "Synchronous orbit" <&> showOrbitalDistance
+    , detailOf bodySphereOfInfluence & titled "Sphere of influence" <&> showOrbitalDistance
+    ]
 
 detailsSections :: [DetailsSection]
 detailsSections = [headerSection, orbitalSection, physicalSection]
